d1bd7868b6512490776dfbbcbf4c9cd2c68123f3
Klaus Seidl <kseidl@recom.eu>
2020-07-16 08:38:19 +0200

    Added super Calculator Engine

---
+++ HelloWorld/HelloWorld/Calculator/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Calculator
+{
+    public class Engine
+    {
+        public double Add(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplizire(double a, double y)
+        {
+            return a + y;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
